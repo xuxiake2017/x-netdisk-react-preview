0f1bc6ccb333b365da52b1b72a0aeada207bfdec
xuxiake2016 <xuxiake2016@163.com>
2020-07-15 19:44:54 +0800

    更改路由为HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import {Provider} from 'react-redux'
 import App from './App'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import store from "./store/store";
 import './style/index.scss'
 
